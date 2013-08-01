d619f0041de2bc354e9b02ee8b683a4f8b72f9bc
Damyon Wiese <damyon@moodle.com>
2013-08-01 16:44:59 +0800

    MDL-36005 formslib: Version bump for css changes

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2012120305.05;              // 20121203      = branching date YYYYMMDD - do not modify!
+$version  = 2012120305.06;              // 20121203      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
