e7ba8902bf2433f4eda2570bb5ee980fd7d70a69
Davo Smith <git@davosmith.co.uk>
2013-08-12 13:51:03 +0800

    MDL-41128 assign: Add feedback_pdf plugin to core.

--- lib/pluginlib.php
+++ lib/pluginlib.php
@@ -664,7 +664,7 @@ class plugin_manager {
             ),
 
             'assignfeedback' => array(
-                'comments', 'file', 'offline'
+                'comments', 'file', 'offline', 'pdf'
             ),
 
             'atto' => array(
