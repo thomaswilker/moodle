4ee1ba393864ebe8ec7415d70e4a191d9b1ac04c
Damyon Wiese <damyon@moodle.com>
2013-09-04 10:53:24 +0800

    MDL-41575 assign: Prevent fatal error with lock/unlock
    This occurs when there is no submission record for a student.

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -4472,7 +4472,7 @@ class assign {
 
         // Give each submission plugin a chance to process the locking.
         $plugins = $this->get_submission_plugins();
-        $submission = $this->get_user_submission($userid, false);
+        $submission = $this->get_user_submission($userid, true);
         foreach ($plugins as $plugin) {
             if ($plugin->is_enabled() && $plugin->is_visible()) {
                 $plugin->lock($submission);
@@ -4508,7 +4508,7 @@ class assign {
 
         // Give each submission plugin a chance to process the locking.
         $plugins = $this->get_submission_plugins();
-        $submission = $this->get_user_submission($userid, false);
+        $submission = $this->get_user_submission($userid, true);
         foreach ($plugins as $plugin) {
             if ($plugin->is_enabled() && $plugin->is_visible()) {
                 $plugin->unlock($submission);
