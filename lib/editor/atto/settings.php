b66e19b127b0a9afea3835d7921e9196bdef55fd
Damyon Wiese <damyon@moodle.com>
2014-02-10 14:03:27 +0800

    MDL-43867 Atto: Remove the keyboard hide and seek from the accessibility helper button.
    It is actually wrong to hide the screen reader helper button from mouse users because of the voiceover/ios
    combination.
    
    We need to support rtl blind touchscreen users!

--- lib/editor/atto/settings.php
+++ lib/editor/atto/settings.php
@@ -30,12 +30,11 @@ $settings = new admin_settingpage('editorsettingsatto', new lang_string('setting
 if ($ADMIN->fulltree) {
     $name = new lang_string('toolbarconfig', 'editor_atto');
     $desc = new lang_string('toolbarconfig_desc', 'editor_atto');
-    $default = 'screenreader = accessibilityhelper' . "\n" .
-               'style = bold, italic, underline, strike, clear, title' . "\n" .
+    $default = 'style = bold, italic, underline, strike, clear, title' . "\n" .
                'paragraph = indent, outdent, orderedlist, unorderedlist' . "\n" .
                'links = link, unlink' . "\n" .
                'insert = table, image, media, managefiles, charmap, emoticon' . "\n" .
-               'other = html, accessibilitychecker';
+               'other = html, accessibilitychecker, accessibilityhelper';
     $setting = new admin_setting_configtextarea('editor_atto/toolbar',
                                                     $name,
                                                     $desc,
