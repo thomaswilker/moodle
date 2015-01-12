a40bc1eafe6560116863ae21de3a8d260aca573a
Adrian Greeve <adrian@moodle.com>
2015-01-12 17:02:05 +0800

    results are interesting

--- mod/lesson/yui/src/pagemmove/js/pagemmove.js
+++ mod/lesson/yui/src/pagemmove/js/pagemmove.js
@@ -15,11 +15,51 @@ function PagemMove() {
     PagemMove.superclass.constructor.apply(this, arguments);
 }
 
+var SELECTORS = {
+    MAINCONTAINER: '.mod_lesson_main',
+    LESSONPAGES: '.mod_lesson_pages',
+    PAGEMODULES: '.mod_lesson_page_element'
+
+    };
+
 Y.namespace('M.mod_lesson').PagemMove = Y.extend(PagemMove, Y.Base, {
 
     initializer: function() {
-        console.log('This works?');
+        var allLessonPages = Y.all(SELECTORS.PAGEMODULES);
+
+        YUI().use('dd-delegate', 'dd-drop-plugin', function(Y) {
+
+            var del = new Y.DD.Delegate( {
+                container: SELECTORS.MAINCONTAINER,
+                nodes: SELECTORS.PAGEMODULES
+
+            });
+
+            del.on('drag:end', function(e) {
+
+                // Keep it in the main container.
+
+                // Get x and y coordinates and update the database.
+
+            });
+
+
+        });
+
+        allLessonPages.each(function (lessonPage) {
+            // Click and drag of the lesson page.
+            lessonPage.on('click', function () {
+                console.log(lessonPage);
+                // console.log(lessonPage.getXY());
+                // lessonPage.setStyle('position', 'absolute');
+                // lessonPage.setStyle('left', '25px');
+                // lessonPage.setStyle('top', '55px');
+
+            });
+        });
+
     }
+
 
 }, {
     NAME: 'pagemMove',
@@ -31,7 +71,7 @@ Y.namespace('M.mod_lesson').PagemMove = Y.extend(PagemMove, Y.Base, {
          * @type Array
          * @writeOnce
          */
-        // tabledata: {
+        // lessondata: {
         //     value: null
         // }
     }
