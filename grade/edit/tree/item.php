6248a43d106cf8f9aa7215c9f7074b8b58330a65
Damyon Wiese <damyon@moodle.com>
2015-08-13 12:16:21 +0800

    MDL-48634 Grades: Make the rescaling option required if maxgrade changes

--- grade/edit/tree/item.php
+++ grade/edit/tree/item.php
@@ -178,7 +178,7 @@ if ($mform->is_cancelled()) {
     } else {
         $grade_item->update();
 
-        if ($data->rescalegrades) {
+        if ($data->rescalegrades == 'yes') {
             $newmin = $grade_item->grademin;
             $newmax = $grade_item->grademax;
             $grade_item->rescale_grades_keep_percentage($oldmin, $oldmax, $newmin, $newmax, 'gradebook');
