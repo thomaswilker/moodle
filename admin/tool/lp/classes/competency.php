2d36116141552b344effaee8db6d964abf8f76fc
Damyon Wiese <damyon@moodle.com>
2015-10-29 12:04:03 +0800

    MDL-51959 cbe: Validate the depth of the path of a competency

--- admin/tool/lp/classes/competency.php
+++ admin/tool/lp/classes/competency.php
@@ -367,7 +367,11 @@ class competency extends persistent {
         } else if (!preg_match('@/([0-9]+/)+@', $value)) {
 
             return new lang_string('invaliddata', 'error');
+        // Validate the depth of the path.
+        } else if ((substr_count($value, '/') - 1) > competency_framework::get_taxonomies_max_level()) {
+            return new lang_string('invaliddata', 'error');
         }
+
 
         return true;
     }
