49628de9f00035854f25bb8c1738b9e027f7b9a6
Damyon Wiese <damyon@moodle.com>
2016-03-17 22:20:55 +0800

    MDL-44078 hooks: Use the callback dispatcher to implement simple hooks

---
+++ lib/classes/hook/hook.php
@@ -0,0 +1,90 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Make hooks dispatchable by providing a get_key method which returns the hook name.
+ *
+ * @package    core
+ * @copyright  2013 Petr Skoda {@link http://skodak.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core\hook;
+
+use core\callback\dispatchable;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Make hooks dispatchable by providing a get_key method which returns the hook name.
+ * Don't call this directly - use \core\callback\hook::fire();
+ *
+ * @package    core
+ * @copyright  2014 Petr Skoda
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+public class hook implements dispatchable {
+
+    /**
+     * @var string The name of the hook
+     */
+    protected $hookname = '';
+
+    /**
+     * @var stdClass|null The arguments passed as an stdClass or null
+     */
+    protected $arguments = null;
+
+    /**
+     * Convenience to create a hook and dispatch it in a single line.
+     *
+     * \core\hook\dispatcher::fire('noseitchy', $args);
+     *
+     * @param string $hookname
+     * @param stdClass $arguments List of modifyable arguments.
+     */
+    public static function fire(string $hookname, \stdClass $arguments = null) {
+        $hook = new static($hookname, $arguments);
+
+        hook_dispatcher::get_instance()->dispatch($hook);
+    }
+
+    /**
+     * Construct a hook compatible with the dispatcher.
+     * Can only be called from fire().
+     *
+     * @param string $hookname
+     * @param stdClass $arguments
+     */
+    protected function __construct($hookname, \stdClass $arguments) {
+        $this->hookname = $hookname;
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * For hooks the key is the hookname.
+     */
+    public function get_key() {
+        return $this->hookname;
+    }
+
+    /**
+     * Hooks get the arguments property passed as an argument.
+     */
+    public function get_arguments() {
+        return $this->arguments;
+    }
+}
---
+++ lib/classes/hook/hook_dispatcher.php
@@ -0,0 +1,120 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Hook class.
+ *
+ * @package    core
+ * @copyright  2014 Petr Skoda {@link http://skodak.org}
+ * @copyright  2016 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace core\hook;
+
+use core\callback\dispatcher_base;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Provide quick and dirty extension points for plugins to intercept and modify data.
+ *
+ * The concepts used here are a "hook" (wrapper containing hook name and args stdClass)
+ * The invoker (The calling code)
+ * The receiver (The callback executed in response to the command)
+ *
+ * Valid hooks must be registered in the hooks array in db/hooks.php for either core, or a plugin.
+ *
+ * Receivers are registered in the receivers array in db/hooks.php. Any plugin or core may
+ * register receivers.
+ *
+ * @package    core
+ * @copyright  2014 Petr Skoda
+ * @copyright  2016 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+public class hook extends dispatcher_base {
+
+    /**
+     * We ignore the optional componentname and throwexceptions for hooks.
+     *
+     * @return dispatchable
+     */
+    public function dispatch(dispatchable $dispatchable, $componentname = null, $throwexceptions = false) {
+        return parent::dispatch($dispatchable);
+    }
+
+    /**
+     * This dispatcher requires commands registered in db/hooks.php.
+     *
+     * @return string
+     */
+    public function get_registration_file_name() {
+        return 'db/hooks.php';
+    }
+
+    /**
+     * The name of the receiever array for commands is '$receivers'
+     *
+     * @return string
+     */
+    public function get_receiver_array_name() {
+        return 'receivers';
+    }
+
+    /**
+     * The name of the dipatchable array is '$hooks'
+     *
+     * @return string
+     */
+    public function get_dispatchable_array_name() {
+        return 'hooks';
+    }
+
+    /**
+     * The name of the cache is 'hookreceivers'
+     *
+     * @return string
+     */
+    public function get_cache_name() {
+        return 'hookreceivers';
+    }
+
+    /**
+     * Checks that hook is listed in lib/db/hooks.php or the db/hooks.php file of a plugin or subsystem.
+     *
+     * This function is only executed in debugging mode.
+     * @param dispatchable $dispatchable
+     */
+    protected static function validate(dispatchable $dispatchable) {
+        global $CFG;
+        if (PHPUNIT_TEST && $component === 'core_tests') {
+            // Ignore commands defined in phpunit fixtures.
+            return;
+        }
+
+        $hookname = $dispatchable->get_key();
+        if (!isset(static::$known[$hookname])) {
+            debugging("Attempt to fire a hook '$hookname' that is not registered in db/hooks.php of any component.",
+                    DEBUG_DEVELOPER);
+        }
+
+        $component = get_dispatchable_component($hookname);
+        if (strcmp($component, $hookname, count($component)) != 0) {
+            debugging("Hook names must begin with the component name. '$hookname' is not correct for component '$component'.",
+                    DEBUG_DEVELOPER);
+        }
+    }
+}
