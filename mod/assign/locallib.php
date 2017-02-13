72ef6a7ebd6faf3be75d51d3274c10cef35102ea
Mark Nelson <markn@moodle.com>
2017-02-13 15:58:06 +0800

    MDL-57938 mod_assign: added new 'gradingduedate' setting
    Part of MDL-55611.

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -635,6 +635,7 @@ class assign {
         }
         $update->duedate = $formdata->duedate;
         $update->cutoffdate = $formdata->cutoffdate;
+        $update->gradingduedate = $formdata->gradingduedate;
         $update->allowsubmissionsfromdate = $formdata->allowsubmissionsfromdate;
         $update->grade = $formdata->grade;
         $update->completionsubmit = !empty($formdata->completionsubmit);
@@ -1241,6 +1242,7 @@ class assign {
         }
         $update->duedate = $formdata->duedate;
         $update->cutoffdate = $formdata->cutoffdate;
+        $update->gradingduedate = $formdata->gradingduedate;
         $update->allowsubmissionsfromdate = $formdata->allowsubmissionsfromdate;
         $update->grade = $formdata->grade;
         if (!empty($formdata->completionunlocked)) {
