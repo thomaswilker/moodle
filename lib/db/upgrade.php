c6f56820de4023a0c51e0c811a19efb7a3835a12
Damyon Wiese <damyon@moodle.com>
2017-03-15 10:48:20 +0800

    MDL-58220 auth_oauth2: Restrict logins to a set of domains
    This is set on the issuer, so you can have different lists for each oauth2 provider.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2630,6 +2630,7 @@ function xmldb_main_upgrade($oldversion) {
         $table->add_field('loginscopesoffline', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
         $table->add_field('loginparams', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
         $table->add_field('loginparamsoffline', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('alloweddomains', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
         $table->add_field('scopessupported', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('showonloginpage', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '1');
         $table->add_field('enabled', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '1');
