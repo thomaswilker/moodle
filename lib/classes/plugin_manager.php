8f8768751fb37aa6b63b96806c1b44274ac52c4c
Damyon Wiese <damyon@moodle.com>
2017-03-16 15:45:22 +0800

    MDL-58280 fileconverter_googledrive: Is born.

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1756,7 +1756,7 @@ class core_plugin_manager {
             ),
 
             'fileconverter' => array(
-                'unoconv'
+                'unoconv', 'googledrive'
             ),
 
             'editor' => array(
